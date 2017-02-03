3b5ec5d09a5543b62c8dd5ebf4a020da2c764933
Rafael Toscano <rltoscano@gmail.com>
2017-02-03 20:00:58 -0200

    Now each page have a dedicated css file. Added task to concatenate them and copy to /build/css directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp');
 var path = require('path');
 var webserver = require('gulp-webserver');
 var clean = require('gulp-clean');
+var concatCss = require('gulp-concat-css');
 
 var CTRL_BASE_PATH = 'control-app';
 
 const paths = {
     images: path.join(CTRL_BASE_PATH, '/img/**/*'),
-    css: path.join(CTRL_BASE_PATH, '/css/**/*'),
+    css: path.join(CTRL_BASE_PATH, '/app/**/*.css'),
+    concatcss: '/css/app.css',
+    build: path.join(CTRL_BASE_PATH, 'build/'),
     libs: [
         path.join(CTRL_BASE_PATH,'node_modules/angular/angular.js'),
         path.join(CTRL_BASE_PATH,'node_modules/angular-ui-router/release/angular-ui-router.min.js'),
@@ -15,21 +18,27 @@ const paths = {
         path.join(CTRL_BASE_PATH,'node_modules/angular-aria/angular-aria.min.js'),
         path.join(CTRL_BASE_PATH,'node_modules/angular-messages/angular-messages.min.js'),
         path.join(CTRL_BASE_PATH,'node_modules/angular-material/angular-material.min.js')
-    ],
-    build: path.join(CTRL_BASE_PATH, 'build/')
+    ]
 };
 
+/* 
+* Clean "build" folder
+*/
 gulp.task('clean', function() {
- return gulp.src(paths.build)
-    .pipe(clean());
+    return gulp.src(paths.build)
+               .pipe(clean());
 });
 
 gulp.task('build', ['clean'], function() {
     // Copy images
     gulp.src(paths.images).pipe(gulp.dest(path.join(CTRL_BASE_PATH, '/build/img')));
 
-    // Copy css
-    gulp.src(paths.css).pipe(gulp.dest(path.join(CTRL_BASE_PATH, '/build/css')));
+    /* 
+    * Get all CSSs files inside "app/", concatenate and copy into /build/css/app.css
+    */
+    gulp.src(paths.css)
+        .pipe(concatCss(paths.concatcss))
+        .pipe(gulp.dest( path.join(paths.build) ))
 
     // Copy external modules
     gulp.src(paths.libs).pipe(gulp.dest(path.join(CTRL_BASE_PATH, '/build/js')));
